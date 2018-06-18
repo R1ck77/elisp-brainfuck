1f86ef86f333270f804642932e2625b58499cdba
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2018-06-18 08:43:44 +0200

    Add empty template for a buffer-backed memory module

---
+++ brainfuck-buffer-memory.el
@@ -0,0 +1,33 @@
+(defvar brainfuck--memory-buffer nil
+  "Memory buffer to hold the buffer evaluation")
+(make-variable-buffer-local 'brainfuck--memory-buffer)
+
+(defun brainfuck--empty-state ()
+  ;;; TODO
+)
+
+(defun brainfuck--init ()
+  ;;; TODO
+)
+
+(defun brainfuck--set (new-value)
+  ;;; TODO
+  )
+
+(defun brainfuck--get ()
+  ;;; TODO
+)
+
+(defun brainfuck--right ()
+  ;;; TODO
+  )
+
+(defun brainfuck--left ()
+  ;;; TODO
+)
+
+(defun brainfuck--print-memory ()
+  ;;; TODO (probably empty)
+)
+
+(provide 'brainfuck-buffer-memory)
